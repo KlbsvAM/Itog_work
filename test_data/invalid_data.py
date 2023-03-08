3a67a4e69edbcdce4511d0da5e6adc867413ae71
KlbsvAM <115699552+KlbsvAM@users.noreply.github.com>
2023-03-08 18:39:04 +0300

    Add files via upload

---
+++ test_data/invalid_data.py
@@ -0,0 +1,12 @@
+# невалидные данные
+
+incorrect_names = ['а', 'z', 'аб', 'AZ', '12345']
+incorrect_surnames = ['а', 'z', 'аб', 'AZ', '12345']
+incorrect_phones = ['812345', '0000000000', '-20', '2.55', '2541263355225545224525']
+incorrect_emails = ['dhfghfh@tdhfg.com', 'hdfghdfh@hfdghdfgh.com']
+incorrect_passwords = ['-456_68ghdfq', 'роывhk', '@@6757uy', '---_=+', 'z']
+incorrect_logins = ['sadsdasdasd', '+76585865856']
+incorrect_phone = '65666667777'
+incorrect_email = 'ashfgh@baghs.unm'
+incorrect_login_yandex = 'dhdh563@ya.rus'
+incorrect_pass = 'hdghdgh56'
---
+++ test_data/valid_data.py
@@ -0,0 +1,7 @@
+# Валидные данные
+
+correct_logins = ['klbsv.a.m@gmail.com', '79023888558']
+correct_pass = 'Qazxsw123q'
+
+agreement_url = 'https://b2c.passport.rt.ru/sso-static/agreement/agreement.html'
+agreement_title = 'Публичная оферта о заключении Пользовательского соглашения на использование Сервиса «Ростелеком ID»'
